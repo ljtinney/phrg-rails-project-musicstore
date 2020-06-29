11293ec8c96e09f43ad448ea9486123f52d98fd3
Liam Tinney <ltinney@powerhrg.com>
2020-06-29 16:00:19 -0400

    Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :customers, only: [:index, :new, :show, :create]
+  resources :instrument, only: [:index, :new, :show, :create]
+  resources :music_store, only: [:index, :new, :show, :create]
+  resources :purchase, only: [:index, :new, :show, :create]
+
 end
