2c35cd185ee94cd0123feb8d18adcf3fa442869a
Liam Tinney <ltinney@powerhrg.com>
2020-06-30 16:35:59 -0400

    Add Secure Password + Validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,6 @@ class Customer < ApplicationRecord
   has_many :purchases
   has_many :instruments, through: :purchases
   has_many :music_stores, through: :purchases
+
+  validates :username, presence: true, uniqueness: true
 end
