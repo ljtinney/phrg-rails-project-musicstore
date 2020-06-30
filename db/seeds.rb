47aaadf66c133564fefa1e417ac0efe9838fbfe0
Liam Tinney <ltinney@powerhrg.com>
2020-06-29 20:47:06 -0400

    Update Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# Heroine.destroy_all
+
 Instrument.destroy_all
 
 instruments = [
@@ -32,27 +32,3 @@ instruments = [
   { instrument_type: "electric bass", name: "JMJ Custom", make: "Fender", model: "Mustang Bass", color: "Daphne Blue", year_made: 2018 },
 
   { instrument_type: "electric bass", name: "Music Man", make: "Ernie Ball", model: "Stingray", color: "Black", year_made: 1972 },
-
-
-
-
-]
-
-# powers.each {|power| Power.create(power)}
-
-# heroines = [
-#   {name: "Kamala Khan", super_name: "Ms. Marvel"},
-#   {name: "Doreen Green", super_name: "Squirrel Girl" },
-#   {name:"Gwen Stacy", super_name:"Spider-Gwen" },
-#   {name:"Janet Van Dyne", super_name:"The Wasp" },
-#   {name:"Wanda Maximoff", super_name:"Scarlet Witch" },
-#   {name:"Carol Danvers", super_name:"Captain Marvel" },
-#   {name:"Jean Grey", super_name:"Dark Phoenix" },
-#   {name:"Ororo Munroe", super_name:"Storm" },
-#   {name:"Shadowcat", super_name:"Kitty Pryde" },
-#   {name:"Elektra", super_name:"Elektra Natchios" }
-# ]
-# heroines = heroines.map { |heroine| heroine.merge( { power_id: Power.all.sample.id } ) }
-# # ---------------
-
-# heroines.each { |heroine| Heroine.create(heroine) }
