8bea519dbd34debc2baf1716b14e8c69d989f828
Liam Tinney <ltinney@powerhrg.com>
2020-07-01 15:48:31 -0400

    Update Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,48 +1,40 @@
-Customer.destroy_all
-Instrument.destroy_all
 
-DATA = {
-  cust_keys: %w[first_name last_name username password],
-  customers: [
-    ["Max", "Charles", "ConeBone69", "password"],
-    ["Katy", "Perry", "KP33", "password"],
-    ["Megan", "Fox", "FoxyLady123", "password"],
-    ["Brian", "May", "KingOfQueen" "password"],
-    ["Jeff", "Buckley","LESpizzaguy" "password"],
-    ["Bruce", "Springsteen", "BossManInUSA", "password"],
-    ["Emma", "Roberts", "NotJulia924", "password"],
-    ["April", "O'Neil", "YellowSuitNews", "password"],
-    ["Julia", "Roberts", "PrettyWomanOceans", "password"],
-    ["Alex", "D'Addario", "IASIPsuperPhan", "password"],
-    ["Seth", "Rogen", "EfficientPineapple", "password"],
-    ["Darth", "Maul", "SithRuleGalaxy3" "password"],
-    ["Nic", "Cage", "PressleyGone34" "password"],
-    ["Sara", "Malakul-Lane", "23Tequillas" "password"],
-  ]
-  instrument_keys: %w[instrument_type name make model color year_made],
-  instruments: [
-["electric guitar", "90's Vintage Rock", "Fender", "Telecaster", "Spotted Atrocious Purple", 1992],
-["electric guitar", "50's American Vintage", "Fender", "Stratocaster", "Daphne Blue", 1956],
-["electric guitar", "Classic", "Gibson", "Les Paul", "Sunburst", 1992],
-["electric guitar", "FSR Two Tone", "Gretsch", "Electromatic Double Jet", "Surf Green & White", 2020],
-["acoustic guitar", "Standard", "Gibson", "Hummingbird", "Cherry Sunburst", 1972],
-["acoustic guitar", "224ce-K DLX", "Taylor", "Grand Auditorium Acoustic-Electric", "Shaded Edge Burst", 2018],
-["acoustic guitar", "D-45 Standard", "Martin", "Dreadnought", "Aged Toner", 2015],
-["acoustic guitar", "17 Series-000-17E", "Martin", "Auditorium Acoustic-Electric", "Whiskey Sunset", 2009],
-["electric bass", "American", "Fender", "Precision Bass", "Vintage Sunburst", 1968],
-["electric bass", "American Ultra", "Fender", "Jazz Bass", "Cobra Blue", 1982],
-["electric bass", "JMJ Custom", "Fender", "Mustang Bass", "Daphne Blue", 2018],
-["electric bass", "Music Man", "Ernie Ball", "Stingray", "Black", 1972],
-]
 
-def make_customers
-  DATA[:customers].each do |customer|
-    new_customer = Customer.new
-    customer.each_with_index do |attribute, i|
-      new_customer.send(DATA[:cust_keys][i] + "=", attribute)
-    end
-    new_customer.save
-  end
-end
+Customer.create!(first_name: "Max", last_name: "Charles", username: "ConeBone69", password: "password")
+Customer.create!(first_name: "Katy", last_name:"Perry", username: "KP33", password: "password")
+Customer.create!(first_name: "Megan", last_name: "Fox", username: "FoxyLady123", password: "password")
+Customer.create!(first_name: "Brian", last_name: "May", username: "KingOfQueen", password:"password")
+Customer.create!(first_name: "Jeff", last_name: "Buckley", username:"LESpizzaguy", password: "password")
+Customer.create!(first_name: "Bruce", last_name: "Springsteen", username: "BossManInUSA", password: "password")
+Customer.create!(first_name: "Emma", last_name: "Roberts", username: "NotJulia924", password: "password")
+Customer.create!(first_name: "April", last_name: "O'Neil", username: "YellowSuitNews", password: "password")
+Customer.create!(first_name: "Keith", last_name: "Richards", username: "ZombieManVinyl", password: "password")
+Customer.create!(first_name: "Alex", last_name: "D'Addario", username: "IASIPsuperPhan", password: "password")
+Customer.create!(first_name: "Seth", last_name: "Rogen", username: "EfficientPineapple", password: "password")
+Customer.create!(first_name: "Darth", last_name: "Maul", username: "SithRuleGalaxy3", password: "password")
+Customer.create!(first_name: "Nic", last_name: "Cage", username: "PressleyGone34", password: "password")
+Customer.create!(first_name: "Sara", last_name: "Malakul-Lane", username: "23Tequillas", password: "password")
+Customer.create!(first_name: "Paul", last_name: "Rudd", username: "Antman2020", password: "password")
+Customer.create!(first_name: "Diana", last_name: "Agron", username: "MumfordFan1105", password: "password")
+Customer.create!(first_name: "Kat", last_name: "Dennings", username: "BrokeGirl1105", password: "password")
+Customer.create!(first_name: "Margot", last_name: "Robbie", username: "OutbackBabe1105", password: "password")
+Customer.create!(first_name: "Tom", last_name: "Hanks", username: "Castaway1105", password: "password")
+Customer.create!(first_name: "Keanu", last_name: "Reeves", username: "IKnowKungFuToo", password: "password")
 
-instruments.each { |instrument_attributes| Instrument.create(instrument_attributes) }
+Instrument.create!(instrument_type: "electric guitar", name: "90's Vintage Rock", make: "Fender", model: "Telecaster", color: "Spotted Atrocious Purple", year_made: 1992)
+Instrument.create!(instrument_type: "electric guitar", name: "50's American Vintage", make: "Fender", model: "Stratocaster", color: "Daphne Blue", year_made: 1956)
+Instrument.create!(instrument_type: "electric guitar", name: "Classic", make: "Gibson", model: "Les Paul", color: "Sunburst", year_made: 1992)
+Instrument.create!(instrument_type: "electric guitar", name: "Brian May Signature", make: "Brian May Guitars", model: "Brian May Signature", color: "Antique Cherry", color: 2016)
+Instrument.create!(instrument_type: "electric guitar", name: "FSR Two Tone", make: "Gretsch", model: "Electromatic Double Jet", color: "Surf Green & White", color: 2020)
+Instrument.create!(instrument_type: "acoustic guitar", name: "Standard", make: "Gibson", model: "Hummingbird", color: "Cherry Sunburst", color: 1972)
+Instrument.create!(instrument_type: "acoustic guitar", name: "224ce-K DLX", make: "Taylor", model: "Grand Auditorium Acoustic-Electric", color: "Shaded Edge Burst", color: 2018)
+Instrument.create!(instrument_type: "acoustic guitar", name: "D-45 Standard", make: "Martin", model: "Dreadnought", color: "Aged Toner", color: 2015)
+Instrument.create!(instrument_type: "acoustic guitar", name: "17 Series-000-17E", make: "Martin", model: "Auditorium Acoustic-Electric", color: "Whiskey Sunset", color: 2009)
+Instrument.create!(instrument_type: "electric bass", name: "American", make: "Fender", model: "Precision Bass", color: "Vintage Sunburst", color: 1968)
+Instrument.create!(instrument_type: "electric bass", name: "American Ultra", make: "Fender", model: "Jazz Bass", color: "Cobra Blue", color: 1982)
+Instrument.create!(instrument_type: "electric bass", name: "JMJ Custom", make: "Fender", model: "Mustang Bass", color: "Daphne Blue", color: 2018)
+Instrument.create!(instrument_type: "electric bass", name: "Music Man", make: "Ernie Ball", model: "Stingray", color: "Black", color: 1972)
+
+MusicStore.create!(name: "GIT YOUR GEETS!", address: "1538 E Passyunk Ave, Philadelphia, PA 19147")
+MusicStore.create!(name: "Triple R Guitar", address: "300 Market St, Lemoyne, PA 17043")
+MusicStore.create!(name: "Emerald City Guitars", address: "83 S Washington St, Seattle, WA 98104")
