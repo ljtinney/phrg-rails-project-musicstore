3dc0bc323464c837450521bc37e218ab89ab713b
Liam Tinney <ltinney@powerhrg.com>
2020-06-30 21:20:02 -0400

    Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/login'
-  get 'sessions/welcome'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :sessions, only: [:new, :create, :destroy]
   resources :customers, only: [:index, :new, :show, :create]
-  resources :instruments, only: [:index, :new, :show, :create]
+  # resources :instruments, only: [:index, :new, :show, :create]
   resources :music_stores, only: [:index, :new, :show, :create]
-  resources :purchases, only: [:index, :new, :show, :create]
+  # resources :purchases, only: [:index, :new, :show, :create]
 
 end
